72fb96faf0fc2388beb66dd1ca4695d31d5e1f8a
phuuthanh2003 <phuuthanh2003@gmail.com>
2024-02-12 22:02:19 +0700

    upload login

---
+++ HuuThanh.FAP.V2.Repositories/UserAccountRepositoryMySQL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuuThanh.FAP.V2.Repositories
+{
+    public class UserAccountRepositoryMySQL : IUserAccountRepository
+    {
+        private List<UserAccount> _users = new List<UserAccount>() 
+        {
+            new UserAccount() {UserName = "phuuthanh2003", Password = "1234"},
+            new UserAccount() {UserName = "jack", Password = "5to?i"},
+        };
+        public UserAccount? Search(string username)
+        {
+            return _users.FirstOrDefault(x =>  x.UserName == username);
+        }
+    }
+}
---
+++ HuuThanh.FAP.V2/Login.cs
@@ -0,0 +1,48 @@
+﻿using HuuThanh.FAP.V2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuuThanh.FAP.V2
+{
+    public partial class Login : Form
+    {
+        private IUserAccountRepository _usesAcc;
+        public Login()
+        {
+            _usesAcc = new UserAccountRepositoryMySQL();
+            InitializeComponent();
+        }
+
+        private void LoginUser(object sender, EventArgs e)
+        {
+
+            UserAccount account = _usesAcc.Search(txtUsername.Text.Trim());
+            if (account != null)
+            {
+                if (account.Password != txtPassword.Text.Trim())
+                {
+                    MessageBox.Show("Invalid password!", "Wrong!!!", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } else
+                {
+                    ListStudents mainForm = new ListStudents();
+                    mainForm.Show();
+                    //this.Close(); //giết cái app luôn
+                    this.Hide();
+                }
+            } else
+            {
+                MessageBox.Show("Invalid username!", "Wrong!!!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
