a001271efc57dcf90e9f3c372ee6dccb38831675
phuuthanh2003 <phuuthanh2003@gmail.com>
2024-02-13 00:42:50 +0700

    update login

---
+++ HuuThanh.FAP.V2.Business/UserAccountService.cs
@@ -0,0 +1,18 @@
+﻿using HuuThanh.FAP.V2.Repositories;
+
+namespace HuuThanh.FAP.V2.Business
+{
+    public class UserAccountService
+    {
+        //UI nhìn: em đưa anh 
+        private IUserAccountRepository _repo = new UserAccountRepositoryMySQL();
+        public bool CheckLogin(string username, string password)
+        {
+            UserAccount userAccount = _repo.Search(username);
+            if( userAccount != null)
+                if (userAccount.Password == password )
+                    return true;
+            return false;
+        }
+    }
+}
